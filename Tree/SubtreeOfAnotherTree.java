7468e35fe6aefb46f3274a75916d1163befe0669
sijia <sijia1231@gmail.com>
2018-07-20 13:52:12 -0700

    add

---
+++ Tree/SubtreeOfAnotherTree.java
@@ -0,0 +1,37 @@
+/**
+Given two non-empty binary trees s and t, check whether tree t has exactly the same structure and node values with a subtree of s. A subtree of s is a tree consists of a node in s and all of this node's descendants. The tree s could also be considered as a subtree of itself.
+检验t是否为s的子树
+**/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public boolean isSubtree(TreeNode s, TreeNode t) {
+        if (t == null) {
+            return true;
+        }
+        if (s == null) {
+            return false;
+        }
+        
+        return isSubtree(s.left,t) || isSubtree(s.right,t) || isSameTree(s,t);        
+    }
+    
+    public boolean isSameTree(TreeNode s, TreeNode t) {
+        if (s == null && t == null) {
+            return true;
+        }
+        if (s == null || t == null || s.val != t.val) {
+            return false;
+        }
+        
+        return isSameTree(s.left, t.left) && isSameTree(s.right, t.right);
+    }
+}
