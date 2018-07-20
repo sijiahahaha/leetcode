dfa2f58fedfb4413484db97e2ce4146f8dd2a623
sijia <sijia1231@gmail.com>
2018-07-19 18:44:46 -0700

    add

--- OA2/#_ThreeSumClosest_16.java
+++ OA2/#_ThreeSumClosest_16.java
@@ -64,3 +64,35 @@ class Solution {
         return bestDiff;
     }
 }
+
+
+
+//要求a+b <= target 并且最接近target
+
+ public static void findOptimalWeights(double capacity, double[] weights, int numOfContainers){
+        double min = 0.0;
+        int minPos = 0;
+        int maxPos = weights.length - 1;
+        int i =0 , j =weights.length-1;
+
+        Arrays.sort(weights);
+
+        while(i < j){
+            double sum = weights[i] + weights[j];
+
+            if(sum > min && sum <= capacity){
+                min = sum;
+                minPos = i;
+                maxPos = j;
+            }
+
+            if(sum > capacity){
+                j--;
+            }else {
+                i++;
+            }
+        }
+
+        System.out.println("The two numbers for which sum is closest to target are "
+                + weights[minPos] + " and " + weights[maxPos]);
+    }
