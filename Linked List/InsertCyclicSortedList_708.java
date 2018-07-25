e9ebb69240ab0e7fb4205d05d347d9951aa26eda
sijia <sijia1231@gmail.com>
2018-07-24 17:45:53 -0700

    add

---
+++ Linked List/InsertCyclicSortedList_708.java
@@ -0,0 +1,55 @@
+/**
+三种情况画出来
+
+**/
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node next;
+
+    public Node() {}
+
+    public Node(int _val,Node _next) {
+        val = _val;
+        next = _next;
+    }
+};
+*/
+class Solution {
+    public Node insert(Node head, int insertVal) {
+        if (head == null) {
+            Node insertNode = new Node(insertVal);
+            insertNode.next = insertNode;
+            return insertNode;
+        }
+        
+         if (head.next == null) {
+            Node insertNode = new Node(insertVal);
+            insertNode.next = head;
+            head.next = insertNode;
+            return head;
+        }
+        
+        Node pre = head;
+        Node next = head.next;
+        
+        do {
+            if (insertVal >= pre.val && insertVal <= next.val) {
+                break;
+            }
+                
+            if ((insertVal < next.val || insertVal > pre.val) && pre.val > next.val) {
+                break;
+            }
+                
+            pre = next;
+            next = next.next;
+        } while (next != head.next);
+        
+        Node insertNode = new Node(insertVal);
+        pre.next = insertNode;
+        insertNode.next = next;
+        return head;    
+    }
+}
