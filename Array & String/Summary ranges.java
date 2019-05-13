42a9af7992f5c311865eeb87ca9f448b176e9426
sijia <sijia1231@gmail.com>
2019-05-12 23:25:56 -0400

    m

---
+++ Array & String/Summary ranges.java
@@ -0,0 +1,35 @@
+/**
+Input:  [0,2,3,4,6,8,9]
+Output: ["0","2->4","6","8->9"]
+ **/
+
+class Solution {
+    public List<String> summaryRanges(int[] nums) {
+        List<String> summary = new List<>();
+        if (nums == null || nums.length == 0) {
+            return summary;
+        }
+        int start = nums[0];
+        int end = start;
+        for (int curr = 1; curr < nums.length; curr++) {
+            if (nums[curr] == nums[curr - 1] + 1) {
+                continue;
+            }
+            end = curr - 1;
+            summary.add(makeRange(start, end, nums));
+            start = curr;
+        }
+        end = nums[nums.length - 1];
+        summary.add(makeRange(start, end, nums));
+        return summary;
+    }
+
+    private String makeRange(int start, int end, int[] nums) {
+        if (start == end) {
+            str = String.format("%d", nums[start]);
+        } else {
+            str = String.format("%d->%d", nums[start], nums[end]);
+        }
+        return str;
+    }
+}
--- DFS/Island Perimeter_463.java
+++ DFS/Island Perimeter_463.java
@@ -1,38 +1,41 @@
 /**
-对于每一个land，跟水或边界相接的一边形成周长 
+对于每一个land，跟水或边界相接的一边形成周长
 ==> 找到每一个land，看四周有几个water或边界
 **/
 
 class Solution {
     public int islandPerimeter(int[][] grid) {
+        int p = 0;
         if (grid == null || grid.length == 0 || grid[0] == null || grid[0].length == 0) {
-            return -1;
+            return p;
         }
-        int perimeter = 0;
+
         int m = grid.length;
         int n = grid[0].length;
-        
+
         int[] dx = {1, 0, -1, 0};
         int[] dy = {0, 1, 0, -1};
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if (grid[i][j] == 1) {
-                    for (int k = 0; k < 4; k++) {
-                        if (water(grid, i + dx[k], j + dy[k])) {
-                            perimeter++;
+                    for (int k = 0; k < 4; k++){
+                        if (isBoundary(i + dx[k], j + dy[k], grid)) {
+                            p += 1;
                         }
                     }
                 }
             }
         }
-        return perimeter;
+
+        return p;
     }
-    private boolean water(int[][] grid, int x, int y) {
+
+    private boolean isBoundary(int x, int y, int[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
-        if (0 <= x && x < m && 0 <= y && y < n) {
-            return grid[x][y] == 0;
+        if (x < 0 || x >= m || y < 0 || y >= n) {
+            return true;
         }
-        return true;
+        return grid[x][y] == 0;
     }
 }
